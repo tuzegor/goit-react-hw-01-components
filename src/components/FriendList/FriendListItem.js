7e832cf827f713bffaabd1621a99dbc81738ea41
Yehor Tuz <tuzegor@gmail.com>
2021-11-29 19:37:01 +0200

    Рефакторинг FriendListItem

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -4,19 +4,11 @@ import s from './FriendListItem.module.css';
 function FriendListItem({ avatar, name, isOnline, id }) {
   return (
     <li className={s.item} id={id}>
-      {isOnline ? (
-        <span
-          className={s.status}
-          width="5"
-          style={{ backgroundColor: 'green' }}
-        ></span>
-      ) : (
-        <span
-          className={s.status}
-          width="5"
-          style={{ backgroundColor: 'red' }}
-        ></span>
-      )}
+      <span
+        className={s.status}
+        width="5"
+        style={{ backgroundColor: isOnline ? 'green' : 'red' }}
+      ></span>
       <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={s.name}>{name}</p>
     </li>
